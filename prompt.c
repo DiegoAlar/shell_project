0638ded01c8899e1452e8083512c37768a9d7c10
Diego <diegoaav@hotmail.com>
2019-11-16 19:58:47 +0000

    adding updates

--- prompt.c
+++ prompt.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+
 #define LSH_TOK_BUFSIZE 64
 #define LSH_TOK_DELIM " \t\r\n\a"
 
 char **split_line(char *line);
-
+void forking(char **splitted);
 int main (void)
 {
       	char *line = NULL;
 	char *ex = "Exit\n";
-	int i = 0;
+	char *com = "l";
+	int i = 0, m = 0;
 	char **splitted = NULL;
 	size_t bufsize = 0;
 
@@ -19,22 +25,26 @@ int main (void)
 		printf("$ ");
 		getline(&line, &bufsize, stdin);
 		if (strcmp(line, ex) == 0)
+		{
+			free(line);
 			return (0);
+		}
 		splitted = split_line(line);
+		printf("%s\n", splitted[0]);
+		if (strcmp(splitted[0], com) == 0)
+		{
+			printf("Entro\n");
+			while (m < 5)
+			{
+				forking(splitted);
+				m++;
+			}
+		}
 		free(splitted);
 		free(line);
 		line = NULL;
 	}
-	/*while (splitted[i])
 
-  	  {
-
-	  printf("%s\n", splitted[i]);
-
-	  i++;
-
-
-	  }*/
 
 
 }
@@ -71,5 +81,27 @@ char **split_line(char *line)
 	}
 	tokens[position] = NULL;
 	return tokens;
+}
+void forking(char **splitted)
+{
+	pid_t child;
+	int status;
+	char *arr[] = {"/bin/ls", "-l", NULL};
+
+	printf("before");
+	child = fork();
+	if (child == 0)
+	{
+		printf("iji");
+		execve(arr[0], arr, NULL);
+		sleep(1);
+	}
+	else
+	{
+		wait(&status);
+
+
+	}
+
 }
 
